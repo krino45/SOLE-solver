46fa7f41493ffbb9fecf800d127cd6064ee37352
krino45 <krina645@gmail.com>
2024-02-06 15:50:42 +0700

    Updated the code to do the modified Gauss version and accept fractions

--- gauss.cpp
+++ gauss.cpp
@@ -6,6 +6,46 @@ typedef struct num {
     int denominator;
 } num;
 
+num mod(num number)
+{   
+    if (number.numerator == 0)
+    {
+        return number;
+    }
+
+    if(number.numerator < 0 && number.denominator > 0)
+    {
+        number.numerator = number.numerator * -1;
+        return number;
+    }
+
+    if(number.numerator > 0 && number.denominator < 0)
+    {
+        number.denominator = number.denominator * -1;
+        return number;
+    }
+
+    if(number.numerator < 0 && number.denominator < 0)
+    {
+        number.numerator = number.numerator * -1;
+        number.denominator = number.denominator * -1;
+        return number;
+    }
+
+    return number;
+}
+
+bool biggerThan(num lhs, num rhs)
+{
+    lhs = mod(lhs);
+    rhs = mod(rhs);
+
+    if (lhs.numerator * rhs.denominator > rhs.numerator * lhs.denominator)
+        return true;
+    else
+        return false;
+}
+
 num inverse(num number)
 {
    if(number.numerator != 0)
@@ -66,20 +106,20 @@ void printarr(num** arr, int row){
             if (arr[i][j].denominator == 1)
             {
                 if(j == 0)
-                    std::cout << std::noshowpos << arr[i][j].numerator << "x";
+                    std::cout << std::noshowpos << " " << arr[i][j].numerator << " * x" << j + 1;
                 else if (j == row)
                     std::cout << std::noshowpos << " = " << arr[i][j].numerator;
                 else
-                    std::cout << std::showpos << arr[i][j].numerator << "x";
+                    std::cout << std::showpos << " " << arr[i][j].numerator << " * x" << std::noshowpos << j+1;
             }
             else 
             {
                 if(j == 0)
-                    std::cout << std::noshowpos << ((arr[i][j].denominator > 0) ? arr[i][j].numerator : ((arr[i][j].numerator > 0) ? (arr[i][j].numerator * -1) : arr[i][j].numerator))<< "/" << std::noshowpos << ((arr[i][j].denominator > 0) ? arr[i][j].denominator : (arr[i][j].denominator * -1)) << "x";
+                    std::cout << std::noshowpos << " " << ((arr[i][j].denominator > 0) ? arr[i][j].numerator : ((arr[i][j].numerator > 0) ? (arr[i][j].numerator * -1) : arr[i][j].numerator))<< "/" << std::noshowpos << ((arr[i][j].denominator > 0) ? arr[i][j].denominator : (arr[i][j].denominator * -1)) << " * x" << j+1;
                 else if (j == row)
                     std::cout << std::noshowpos << " = " << ((arr[i][j].denominator > 0) ? arr[i][j].numerator : ((arr[i][j].numerator > 0) ? (arr[i][j].numerator * -1) : arr[i][j].numerator))<< "/" << std::noshowpos << ((arr[i][j].denominator > 0) ? arr[i][j].denominator : (arr[i][j].denominator * -1)) ;
                 else
-                    std::cout << std::showpos << ((arr[i][j].denominator > 0) ? arr[i][j].numerator : ((arr[i][j].numerator > 0) ? (arr[i][j].numerator * -1) : arr[i][j].numerator))<< "/" << std::noshowpos << ((arr[i][j].denominator > 0) ? arr[i][j].denominator : (arr[i][j].denominator * -1))  << "x";
+                    std::cout << std::showpos << " " << ((arr[i][j].denominator > 0) ? arr[i][j].numerator : ((arr[i][j].numerator > 0) ? (arr[i][j].numerator * -1) : arr[i][j].numerator))<< "/" << std::noshowpos << ((arr[i][j].denominator > 0) ? arr[i][j].denominator : (arr[i][j].denominator * -1))  << " * x" << j+1;
             }
 
         }
@@ -93,20 +133,20 @@ void swapRow(num** arr, int columns, int rowindex1, int rowindex2)
     num temp;
     for (int i = 0; i < columns; i++)
     {
-        arr[rowindex1][i] = temp;
+        temp = arr[rowindex1][i];
         arr[rowindex1][i] = arr[rowindex2][i];
         arr[rowindex2][i] = temp;
     }
 
 }
 
-int GaussForwardStep(num** arr, int row)
+int GaussForwardStep(num** arr, int row, int modified)
 {
     int columns = row + 1;
     for (int i = 0; i < row - 1; i++)
     {
-        if (arr[i][i].denominator == 0)
-        {
+        if (arr[i][i].numerator == 0)
+        {   
             int flag = 1;
             for (int pivotFind = 1; pivotFind < row; pivotFind++)
             {
@@ -124,6 +164,27 @@ int GaussForwardStep(num** arr, int row)
                 std::cout << "The matrix has no solutions (det == 0)";
                 return 0;
             }
+        }
+        if (modified && i < (row - 1))
+        {
+            num biggest = arr[i][i];
+            int rowindexmodified = i;
+
+            for (int k = i; k < row; k++)
+            {
+                if(biggerThan(arr[k][i], biggest))
+                {
+                    biggest = arr[k][i];
+                    rowindexmodified = k;
+                }
+            }
+            if(rowindexmodified != i)
+            {
+                swapRow(arr, columns, i, rowindexmodified);
+                std::cout << "Swapped rows " << i+1 << " and " << rowindexmodified+1 << "\n";
+                printarr(arr, row);
+            }
+
         }
         num placeholder = arr[i][i];
         for (int j = 0; j < columns; j++)
@@ -145,7 +206,15 @@ int GaussForwardStep(num** arr, int row)
     }
     std::cout << "Square SOLE after forwards-Gauss: \n";
     printarr(arr, row);
-    return 1;
+    for (int i = 0; i < row; i++)
+    {
+        if(arr[i][i].numerator == 0)
+        {
+            std::cout << "The matrix has no solutions (det == 0)";
+            return 0;
+        }
+    }
+        return 1;
 }
 
 void GaussBackwardsStep(num** arr, int row)
@@ -160,10 +229,15 @@ void GaussBackwardsStep(num** arr, int row)
             }
         if (i != 0)
         {
-            if(arr[i-1][i].numerator != 0)
+            for (int j = i; j > 0; j--)
             {
-                arr[i - 1][columns - 1] = subtract(arr[i - 1][columns - 1], multiply(arr[i - 1][i], arr[i][columns - 1]));
+                if(arr[j-1][i].numerator != 0)
+                {
+                    arr[j - 1][columns - 1] = subtract(arr[j - 1][columns - 1], multiply(arr[j - 1][i], arr[i][columns - 1]));
+                }
+                arr[j - 1][i].numerator = 0;
             }
+
         }
         
     }
@@ -172,11 +246,14 @@ void GaussBackwardsStep(num** arr, int row)
     printarr(arr, row);
 }
 
-void Gauss(num** arr, int row){
+int Gauss(num** arr, int row, int modifiedflag){
     int returncodecheck = 1;
-    returncodecheck = GaussForwardStep(arr, row);
+    returncodecheck = GaussForwardStep(arr, row, modifiedflag);
     if(returncodecheck) 
         GaussBackwardsStep(arr, row);
+    else
+        return 0;
+    return 1;
 }
 
 int main(){
@@ -197,34 +274,81 @@ int main(){
 
     int temp1 = 0;
     int temp2 = 0;
+    int fractionFlag = 2;
+    int modifiedflag = 0;
+    std::cout << "\n\nModidied Gauss Method?\n\n\n1) Yes\n\n2) No\n\n";
+    std::cin >> modifiedflag;
+    modifiedflag = (modifiedflag == 1) ? 1 : 0;
+
+    std::cout << "\n\nDo you want to input the coefficients in the fraction form?\n\n\n1) Yes\n\n2) No\n\n";
+    std::cin >> fractionFlag;
+
+    
     for (int i = 0; i < row; i++)
     {
         temp1 = i + 1;
         for (int j = 0; j < column; j++){
             temp2 = j + 1;
-            std::cout << "Eq." << temp1 << ") Input the coefficient before the x #" << temp2 <<".\n";
-            std::cin >> arr[i][j].numerator;
-            arr[i][j].denominator = 1;
+            if(fractionFlag == 1)
+            {
+                if(j == column - 1)
+                {
+                    std::cout << "Eq." << temp1 << ") Input the solution's numerator.\n";
+                    std::cin >> arr[i][j].numerator;
+                    std::cout << "Eq." << temp1 << ") Input the solution's denomerator.\n";
+                    std::cin >> arr[i][j].denominator;
+                }
+                else
+                {
+                    std::cout << "Eq." << temp1 << ") Input the numerator of the coefficient before the x #" << temp2 <<".\n";
+                    std::cin >> arr[i][j].numerator;
+                    std::cout << "Eq." << temp1 << ") Input the denominator of the coefficient before the x #" << temp2 <<".\n";
+                    std::cin >> arr[i][j].denominator;
+                }
+                if (arr[i][j].denominator == 0)
+                {
+                    std::cout << "Invalid denominator Input! Stopping...";
+                   
+                    return 0;
+                }
+            }
+            else
+            {
+                if(j == column - 1)
+                {
+                    std::cout << "Eq." << temp1 << ") Input the solution for the equation.\n";
+                    std::cin >> arr[i][j].numerator;
+                }
+                else
+                {
+                    std::cout << "Eq." << temp1 << ") Input the coefficient before the x #" << temp2 <<".\n";
+                    std::cin >> arr[i][j].numerator;
+                }
+                arr[i][j].denominator = 1;
+            }
         }
     }
 
     std::cout << "Provided square SOLE: \n";
     printarr(arr, row);
 
-    Gauss(arr, row);
-
-    std::cout << "Solutions for the SOLE: \n";
-    for (int i = 0; i < row; i++)
+    if(Gauss(arr, row, modifiedflag))
     {
-        if(arr[i][row].denominator == 1)
-            std::cout << "x" << i + 1 << " = " << arr[i][row].numerator << std::endl; // row - last column of the matrix i know its not intuitive so what
-        else
-            std::cout << "x" << i + 1 << " = " << arr[i][row].numerator << "/" << arr[i][row].denominator << std::endl;
-    }
-
+        std::cout << "Solutions for the SOLE: \n";
         for (int i = 0; i < row; i++)
         {
-            delete arr[i];
+            if(arr[i][row].denominator == 1)
+                std::cout << "x" << i + 1 << " = " << arr[i][row].numerator; // row - last column of the matrix i know its not intuitive so what
+            else // help
+                std::cout << "x" << i + 1 << " = " << ((arr[i][row].denominator > 0) ? arr[i][row].numerator : ((arr[i][row].numerator > 0) ? (arr[i][row].numerator * -1) : arr[i][row].numerator))<< "/" << std::noshowpos << ((arr[i][row].denominator > 0) ? arr[i][row].denominator : (arr[i][row].denominator * -1)) << std::endl; 
         }
+    }
+
+    for (int i = 0; i < row; i++)
+    {
+        delete arr[i];
+    }
     delete arr;
+
+    return 0;
 }
